7811516f67ae11c2325de84f0b122689f3ec2462
TarunM <mauryatarun71@gmail.com>
2026-04-01 00:56:59 +0530

    Updated file after edits

--- router/router.js
+++ router/router.js
@@ -1,43 +1,95 @@
+// import express from 'express'
+// import jwt from 'jsonwebtoken'
+// import { resisterController,loginController,createTodoController
+//     ,getTodoController,deleteTodoController,updateTodoController,userController
+//  } from '../controller/resisterController.js';
+// const router=express.Router();
+// router.post("/resister",resisterController);
+// router.post("/login",loginController);
+// const AuthMiddleware=(req,res,next)=>{
+//     if(!req.headers.authorization){
+//          return({
+//                 success:false,
+//                 code:400,
+//                 message:"token is required",
+//                 data:"",
+//                 error:true
+//          })
+//     }
+//     const rawToken=req?.headers?.authorization?.split(" ");
+//     const token=rawToken[1];
+//     jwt.verify(token,"jwt_secret",(err,decode)=>{
+//         if(err){
+//             return res.json({
+//                 success:false,
+//                 code:400,
+//                 message:"invalid or expired token",
+//                 data:"",
+//                 error:true
+//             })
+//         }else{
+//             req.user=decode;
+//             next()
+//         }
+//     })
+    
+// }
+// router.post("/create-todo",AuthMiddleware,createTodoController)
+// router.get("/get-todo",AuthMiddleware,getTodoController)
+// router.delete("/del-todo/:id",AuthMiddleware,deleteTodoController)
+// router.put("/update-todo/:id",AuthMiddleware,updateTodoController)
+// router.get('/userData',userController)
+
+// export default router;
+
 import express from 'express'
 import jwt from 'jsonwebtoken'
-import { resisterController,loginController,createTodoController
-    ,getTodoController,deleteTodoController,updateTodoController,userController
- } from '../controller/resisterController.js';
-const router=express.Router();
-router.post("/resister",resisterController);
-router.post("/login",loginController);
-const AuthMiddleware=(req,res,next)=>{
-    if(!req.headers.authorization){
-         return({
-                success:false,
-                code:400,
-                message:"token is required",
-                data:"",
-                error:true
-         })
+import { resisterController, loginController, createTodoController,
+    getTodoController, deleteTodoController, updateTodoController, userController
+} from '../controller/resisterController.js';
+
+const router = express.Router();
+
+// ✅ Root route added
+router.get("/", (req, res) => {
+    res.send("API is running successfully!");
+});
+
+router.post("/resister", resisterController);
+router.post("/login", loginController);
+
+const AuthMiddleware = (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.json({   // ✅ yahan res.json use karo
+            success: false,
+            code: 400,
+            message: "token is required",
+            data: "",
+            error: true
+        });
     }
-    const rawToken=req?.headers?.authorization?.split(" ");
-    const token=rawToken[1];
-    jwt.verify(token,"jwt_secret",(err,decode)=>{
-        if(err){
+    const rawToken = req?.headers?.authorization?.split(" ");
+    const token = rawToken[1];
+    jwt.verify(token, "jwt_secret", (err, decode) => {
+        if (err) {
             return res.json({
-                success:false,
-                code:400,
-                message:"invalid or expired token",
-                data:"",
-                error:true
-            })
-        }else{
-            req.user=decode;
-            next()
+                success: false,
+                code: 400,
+                message: "invalid or expired token",
+                data: "",
+                error: true
+            });
+        } else {
+            req.user = decode;
+            next();
         }
-    })
-    
-}
-router.post("/create-todo",AuthMiddleware,createTodoController)
-router.get("/get-todo",AuthMiddleware,getTodoController)
-router.delete("/del-todo/:id",AuthMiddleware,deleteTodoController)
-router.put("/update-todo/:id",AuthMiddleware,updateTodoController)
-router.get('/userData',userController)
+    });
+};
+
+router.post("/create-todo", AuthMiddleware, createTodoController);
+router.get("/get-todo", AuthMiddleware, getTodoController);
+router.delete("/del-todo/:id", AuthMiddleware, deleteTodoController);
+router.put("/update-todo/:id", AuthMiddleware, updateTodoController);
+router.get("/userData", userController);
 
 export default router;
